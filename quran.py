c4877f59e12e3dfd16c57692cc593a71dfffe49c
malick <malickgning14@gmail.com>
2024-08-11 18:09:58 +0200

    change code creation of full page for quran

--- quran.py
+++ quran.py
@@ -1,18 +1,25 @@
 import sys; sys.path.append('.')
 
+from argparse import ArgumentParser
 from tqdm import tqdm
 import concurrent.futures
 
-from src.notion import Notion
+# todo: quran command
+from src.surah import Surah
 
-def main():
+def main(args):
 
     futures = []
 
     with concurrent.futures.ThreadPoolExecutor() as executor:
         for i in tqdm(range(1,115), desc="Surah crawling"):
-            futures.append(executor.submit(Notion().create_surah, Surah(i)))
+            surah = Surah(i)
+            futures.append(executor.submit(surah.create))
         for future in concurrent.futures.as_completed(futures):
             future.result()
 
-if __name__ =="__main__": main()
+if __name__ =="__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--num", "-n", required=True, type=int)
+    args = parser.parse_args()
+    main(args)
